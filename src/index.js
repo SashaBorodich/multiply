fc348fddf42b7c694a55c11d223f6269d5490b67
Alexander Borodich <sasha.barodach@gmail.com>
2019-09-24 21:19:26 +0300

    feat: implement function Multiply

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function multiply(first, second) {
-  // your solution
+	
+	let result = 0; //declare variable
+
+	result = BigInt(first) * BigInt(second);	// convert numbers to BigInt
+
+	return result.toString(); // convert result to String
+
 }
